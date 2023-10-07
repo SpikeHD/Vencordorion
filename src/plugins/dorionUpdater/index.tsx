95a519e852d5361225b99c5761d547d1d53456bb
SpikeHD <spikegdofficial@gmail.com>
2023-10-07 16:19:00 -0700

    fix: use alerts stuff

--- src/plugins/dorionUpdater/index.tsx
+++ src/plugins/dorionUpdater/index.tsx
@@ -6,6 +6,7 @@
 
 import { Devs } from "@utils/constants";
 import definePlugin from "@utils/types";
+import { Alerts } from "@webpack/common";
 
 export default definePlugin({
     name: "Dorion Updater",
@@ -18,18 +19,32 @@ export default definePlugin({
         const update_check = await window.__TAURI__.invoke("update_check");
         const { autoupdate } = JSON.parse(await window.__TAURI__.invoke("read_config_file"));
 
+        const doUpdate = () => {
+            window.__TAURI__.invoke("do_update", {
+                toUpdate: update_check,
+            });
+        };
+
         console.log(`Dorion things to update: ${update_check}`);
 
         if (update_check.includes("vencordorion") || update_check.includes("dorion")) {
-            const updateConfirm = autoupdate || await window.__TAURI__.dialog.confirm(
-                "There are updates to Dorion available. Would you like to update?\n\nYou can enable auto-updates in Dorion settings.",
-            );
-
-            if (updateConfirm) {
-                await window.__TAURI__.invoke("do_update", {
-                    toUpdate: update_check,
-                });
+            // If autoupdate is enabled, just do it, otherwise ask the user.
+            if (autoupdate) {
+                doUpdate();
+                return;
             }
+
+            Alerts.show({
+                title: "Updates Available!",
+                body: (
+                    <>
+                        <p>There are Dorion-related updates available. Would you like to apply them?</p>
+                    </>
+                ),
+                confirmText: "Yes please!",
+                cancelText: "Nope!",
+                onConfirm: () => doUpdate()
+            });
         }
     }
 });
