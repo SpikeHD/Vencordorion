3158174911b076332c46a7a6c7dddff59e4f517c
banaanae <will.litch@outlook.com>
2023-10-08 19:30:28 +1100

    Revert "fix: web cache confirmation"
    This reverts commit 7afc956d714b0fc00b54cd0be894d7362ea2e452.

--- src/components/DorionPerformance/DorionPerformance.tsx
+++ src/components/DorionPerformance/DorionPerformance.tsx
@@ -19,7 +19,7 @@
 import { SettingsTab, wrapTab } from "@components/VencordSettings/shared";
 import { Margins } from "@utils/margins";
 import { classes } from "@utils/misc";
-import { Alerts, Button, Forms, React, showToast, Switch, useEffect, useState } from "@webpack/common";
+import { Button, Forms, React, showToast, Switch, useEffect, useState } from "@webpack/common";
 
 const { invoke, process, dialog } = window.__TAURI__;
 
@@ -65,18 +65,12 @@ function DorionSettingsTab() {
     };
 
     const clearWebCache = async () => {
-        Alerts.show({
-            title: "Are you sure?",
-            body: (
-                <>
-                    <p>Clearing web cache will log you out and reset your Vencord settings (unless they are on the cloud, of course), but can often help solve permission-based issues.</p>
-                    <p>Do you want to proceed?</p>
-                </>
-            ),
-            confirmText: "Confirm",
-            cancelText: "Cancel",
-            onConfirm: () => invoke("set_clear_cache")
-        });
+        const doClear = await dialog.confirm(
+            "Are you sure you want to clear the web cache? This will log you out and close Dorion.",
+            "Clear Web Cache",
+        );
+
+        if (doClear) await invoke("set_clear_cache");
     };
 
     return (
